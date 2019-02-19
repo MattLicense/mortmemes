9527183555a0b28cfcadfa24fdef5b14b4a00432
Philip Herron <herron.philip@googlemail.com>
2019-02-19 09:47:42 +0000

    Initial spring boot

---
+++ src/test/java/org/mortmemes/mortology/MortologyApplicationTests.java
@@ -0,0 +1,16 @@
+package org.mortmemes.mortology;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class MortologyApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
